980c1a489a3288ba102eb88584e3b745512f0196
Kevan <kevan.haggins@gmail.com>
2023-03-16 12:34:59 -0700

    Very simple server that renders different html pages based on the url request

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
-// localhost:8080 take users to index.html
-//  localhost:8080/about take users to about.html
-// localhost:8080/contact-me
-// 404.html if none of the above
+const http = require("http");
+const fs = require("fs");
 
-const https = require("https");
+const server = http.createServer((req, res) => {
+  const url = req.url;
+  if (url == "/") {
+    fs.readFile("./index.html", (err, html) => {
+      if (err) {
+        throw err;
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(html);
+      return res.end();
+    });
+  } else if (url == `/about`) {
+    fs.readFile("./about.html", (err, html) => {
+      if (err) {
+        throw err;
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(html);
+      return res.end();
+    });
+  } else if (url == "/contact-me") {
+    fs.readFile("./contact-me.html", (err, html) => {
+      if (err) {
+        throw err;
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(html);
+      return res.end();
+    });
+  } else {
+    fs.readFile("./404.html", (err, html) => {
+      if (err) {
+        throw err;
+      }
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.write(html);
+      return res.end();
+    });
+  }
+});
+
+server.listen(8080);
